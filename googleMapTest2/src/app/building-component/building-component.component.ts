cc8d0f3d4ae9a47039d685ee2283e64e0027015c
corentindargent <xx-cox-xx@hotmail.com>
2017-05-09 17:02:24 +0200

    Correction ergonomie modif spaces

--- googleMapTest2/src/app/building-component/building-component.component.ts
+++ googleMapTest2/src/app/building-component/building-component.component.ts
@@ -301,12 +301,15 @@ export class BuildingComponent implements OnInit {
 	this.selectedSpace = polygon;
 	this.selectedSpace.setAttribute('fill', '#00FE08');
 	 
-/* 
-inserer le polygn a la fin pour tre entierement visible
-	var g = polygon.parentNode.parentNode;
-	
-	g.insertBefore(polygon.parentNode,g.lastChild);
-	 */
+
+	/* on place le parent du polygon a la fin des enfants du "grand - pere" du polygon. 
+	Pour qu'a la modif le polygon selectionne soit sur le premier plan.
+	Sans cela en fct de la pos dasn arboresence dom l'edition du polygon sera + complique.
+	*/ 
+	 var g = polygon.parentNode.parentNode;
+	 console.log(g);
+	 g.appendChild(polygon.parentNode);
+	 
 	 //garnit la varaible d'update avec les info du polygon selectionne
 	this.spaceUpdate.reference = this.currentFloor.listSpaces[this.selectedSpace.id].reference;
     this.spaceUpdate.id_space = this.currentFloor.listSpaces[this.selectedSpace.id].id_space;
